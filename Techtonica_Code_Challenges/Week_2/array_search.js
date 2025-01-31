0bbcad3aeb7cd5f6fe8d413a9310a15071be08b2
Daaimah Tibrey <daaimah12392@gmail.com>
2025-01-31 08:38:35 -0800

    KC3

--- Techtonica_Code_Challenges/Week_2/array_search.js
+++ Techtonica_Code_Challenges/Week_2/array_search.js
@@ -8,4 +8,35 @@ console.log(findLastIndex([10, 20, 30, 40, 50], 25));  // Output: -1
 console.log(findLastIndex([5, 10, 15, 20, 10, 25], 10));  // Output: 4
 console.log(findLastIndex([1, 2, 3, 4, 5], 6));  // Output: -1
 
+Input
+first param => array of integers, 
+second param => target: the element to search for in the array
+
+Output
+returning the last index of the target element in the array, if not found return -1
+
+
 */
+
+function findLastIndex(arr, target){
+    const lastIndex = arr.findLastIndex((el) => el === target)
+    /*
+    
+    arr: [1, 2, 3, 4, 5, 2]  
+    iteration 1: element = 2....if 2 === 2; length (6) - 1 = 5
+    target: 2
+
+    */
+    //     if-condition  ?   true-condition  : false-condition
+    return lastIndex !== -1 ? lastIndex : -1;
+    // if(lastIndex !== -1){
+    //     return lastIndex;
+    // } else {
+    //     return -1;
+    // }
+}
+
+console.log(findLastIndex([1, 2, 3, 4, 5, 2], 2));  // Output: 5
+console.log(findLastIndex([10, 20, 30, 40, 50], 25));  // Output: -1
+console.log(findLastIndex([5, 10, 15, 20, 10, 25], 10));  // Output: 4
+console.log(findLastIndex([1, 2, 3, 4, 5], 6)); // Output: -1
