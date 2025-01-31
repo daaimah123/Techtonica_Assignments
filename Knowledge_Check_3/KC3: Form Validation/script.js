5056ed2faf007122d980f7887e01aca37dc10130
Daaimah Tibrey <41805952+daaimah123@users.noreply.github.com>
2025-01-31 09:00:57 -0800

    Update script.js

--- Knowledge_Check_3/KC3: Form Validation/script.js
+++ Knowledge_Check_3/KC3: Form Validation/script.js
@@ -28,13 +28,10 @@ Activate click on submit ID and conditionally do these things
 */
 
 // make success icons invisible
-const successIcon = document.getElementsByClassName("far fa-check-circle success-icon");
-for (let i = 0; i < successIcon.length; i++){
-  successIcon[i].style.visibility = 'hidden'
-};
+const successIcon 
 
 // access the failure icon
-const failureIcon = document.getElementsByClassName("fas fa-exclamation-circle failure-icon");
+const failureIcon 
 
 // valid email
 const correctEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -57,50 +54,23 @@ document.getElementById('submit').addEventListener('click',
         passwordError.style.color = 'red'
 
         // Handle user inputs on user input area id (username, password, email), then grab value
-        const username = document.getElementById('username').value;
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
+        const username 
+        const email 
+        const password 
 
         // Failure icon visible upon empty form submission using the error class, change icon to red
         // username => failure_icon ? empty : success_icon
-        if (username === ''){
-          userNameError.innerText = "Please enter field"
-        }
+        
         // email => failure_icon ? zero length or not meeting the pattern : success_icon
-        if (email === ''){
-          emailError.innerText = "Please enter field"
-        }
+        
         // password => failure_icon ? is < 8 chars : success_icon
-        if (password === ''){
-          passwordError.innerText = "Please enter field"
-        }
-
-        if (username !== '' && email !== "" && password !== ""){
-          userNameError.innerText = ''; 
-          // making success visible
-          successIcon[0].style.visibility = 'visible';
-          // make error hidden
-          failureIcon[0].style.visibility = 'hidden';
-        }
+       
 
         // email is valid
-        if (correctEmail.test(email)){
-          emailError.innerText = ''; 
-          // making success visible
-          successIcon[1].style.visibility = 'visible';
-          // make error hidden
-          failureIcon[1].style.visibility = 'hidden';
-        }
+       
 
         // password at least 8 characters
-        if (password.length < 8){
-          passwordError.innerText = ''; 
-          // making success visible
-          successIcon[1].style.visibility = 'visible';
-          // make error hidden
-          failureIcon[1].style.visibility = 'hidden';
-        }
-
+        
         
       }
 )
